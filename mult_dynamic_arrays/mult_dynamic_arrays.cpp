ca5f27370e727012dc824279e411a8f96c890b20
Jinyu Liu <149179560+JinyuLiu0116@users.noreply.github.com>
2024-07-05 22:28:38 -0400

    Add files via upload

---
+++ mult_dynamic_arrays/mult_dynamic_arrays.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+#include <ctime>
+using namespace std;
+
+int main()
+{
+	srand(time(NULL));
+	int row, colum;
+	cout << "Enter the number of rows:";
+	cin >> row;
+	cout << "Enter the number of colum:";
+	cin >> colum;
+
+	//creat a pointer to a pointer has row many elements.
+	int** ptopTable = new int*[row];// int*[] declear that we not just creat a dynamic array but array of pointer.
+	for (int index = 0; index < row; index++)
+	{	//assign the address of dynamic arrays with colum number of elements to pointer
+		ptopTable[index] = new int[colum];//array is address of its first index member, so it can directly signt to pointer
+	}
+	//assign a value to row 1, colum 2
+	ptopTable[1][2] = 88;
+	//use nest loop and rand function to sign values to fillfull ptopTable
+	for (int rows = 0; rows < row; rows++)
+	{
+		for (int cols = 0; cols < colum; cols++)
+		{
+			ptopTable[rows][cols] = rand() % 20 + 1;
+		}
+	}
+	for (int rows = 0; rows < row; rows++)
+	{
+		for (int cols = 0; cols < colum; cols++)
+		{
+			cout << ptopTable[rows][cols] << " ";
+		}
+		cout << '\n';
+	}
+	//every time we use 'new' to create a memory, we need to use 'delete' when we don't need the memory
+	//for mult-dynamic arrays, we need to delete upsit way: from the end to the begining.
+	for (int index = 0; index < row; index++)
+	{
+		delete[] ptopTable[index];//delete three dynamic arrays
+	}
+	delete[] ptopTable;//delete the dynamic array that holds three address, after that ptopTable point to a unused memory.
+	ptopTable = NULL;//point to NULL
+	
+
+
+
+}
