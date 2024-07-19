b22bf16d9502c753009c1754e4b6b07871249e17
Jinyu Liu <149179560+JinyuLiu0116@users.noreply.github.com>
2024-07-19 13:13:29 -0400

    Add files via upload

--- HW_4.cpp
+++ HW_4.cpp
@@ -1,27 +1,29 @@
 #include <iostream>
 #include <string>
 using namespace std;
-const int SIZE =1;
+const int SIZE = 5;
 
 class LaGuardiaStudent
 {
 public:
     string name;
     string email;
-    string student="LaGuardia";
+    string student = "LaGuardia";
     int fistTHreeIndex;
     int secondTheeIndex;
     int lastFourIndex;
-    LaGuardiaStudent(){
-        this->student="LaGuardia";
+    LaGuardiaStudent() {
+        this->student = "LaGuardia";
         this->name = "";
         this->email = "";
         this->fistTHreeIndex = 0;
         this->secondTheeIndex = 0;
         this->lastFourIndex = 0;
     }
+    //after created array of object, use this function to set valuses
     void setInformation(string, string, int, int, int);
-    void display(int i);
+    void display();
+    //geters
     string getName();
     string getEmail();
     string getStudent();
@@ -32,160 +34,170 @@ public:
 class ResidentStudent : public LaGuardiaStudent
 {
 public:
-    ResidentStudent() : LaGuardiaStudent(){
-        this->student="Resident";
+    //inheric from parent class and hardcode student variable
+    ResidentStudent() : LaGuardiaStudent() {
+        this->student = "Resident";
     };
-    void display(int i);
+    //polymorphism function
+    void display();
 };
 
 class InternationalStudent : public LaGuardiaStudent
 {
 public:
-    InternationalStudent() : LaGuardiaStudent(){
-        this->student="International";
+    InternationalStudent() : LaGuardiaStudent() {
+        this->student = "International";
     };
-    void display(int i);
+    void display();
 };
-int getDigit(int num){
-    int digit=0;
-    while(num>0){
-        num/=10;
+//return the number of digits of the number
+int getDigit(int num) {
+    int digit = 0;
+    while (num > 0) {
+        num /= 10;
         digit++;
     }
     return digit;
 }
+//overload functions to take user input
 void fillFullInfor(ResidentStudent[]);
 void fillFullInfor(InternationalStudent[]);
-int main(){
+int main() {
     ResidentStudent Rstudent[SIZE];
     InternationalStudent Istudent[SIZE];
 
-    int index=0;
     cout << "Welcom to use Information System\n";
-	cout << "Please fill the information for LaGuardia college student:\n";
-	cout << "-----------------------------------------------------------\n";
-	cout << "|    Student    | Full name |    PhoneNumber   |     Email      |\n";
-	cout << "-----------------------------------------------------------\n";
+    cout << "Please fill the information for LaGuardia college student:\n";
+    cout << "-----------------------------------------------------------------\n";
+    cout << "|    Student    | Full name |    PhoneNumber   |     Email      |\n";
+    cout << "-----------------------------------------------------------------\n";
+    //call function to get user input and assign to object
     fillFullInfor(Rstudent);
-    cin.ignore();
-    cin.clear();
-    fillFullInfor(Istudent);
     
-    cout << "-----------------------------------------------------------\n";
-	cout << "|    Student    | Full name |    PhoneNumber   |     Email      |\n";
-	cout << "-----------------------------------------------------------\n";
-    for(int i=0;i<SIZE;i++)
-        Rstudent[i].display(i);
-    for(int i=0;i<SIZE;i++)
-        Istudent[i].display(i);
+    fillFullInfor(Istudent);
+
+    cout << "-----------------------------------------------------------------\n";
+    cout << "|    Student    | Full name |    PhoneNumber   |     Email      |\n";
+    cout << "-----------------------------------------------------------------\n";
+    //display information
+    for (int i = 0; i < SIZE; i++)
+        Rstudent[i].display();
+    for (int i = 0; i < SIZE; i++)
+        Istudent[i].display();
 }
-void LaGuardiaStudent::setInformation(string name, string email, int first, int second, int last){
+void LaGuardiaStudent::setInformation(string name, string email, int first, int second, int last) {
     this->name = name;
     this->email = email;
     this->fistTHreeIndex = first;
     this->secondTheeIndex = second;
     this->lastFourIndex = last;
 }
-string LaGuardiaStudent::getStudent(){
+string LaGuardiaStudent::getStudent() {
     return this->student;
 }
-string LaGuardiaStudent::getName(){
+string LaGuardiaStudent::getName() {
     return this->name;
 }
-string LaGuardiaStudent::getEmail(){
+string LaGuardiaStudent::getEmail() {
     return this->email;
 }
-int LaGuardiaStudent::getFirst(){
+int LaGuardiaStudent::getFirst() {
     return this->fistTHreeIndex;
 }
-int LaGuardiaStudent::getSecond(){
+int LaGuardiaStudent::getSecond() {
     return this->secondTheeIndex;
 }
-int LaGuardiaStudent::getLast(){
+int LaGuardiaStudent::getLast() {
     return this->lastFourIndex;
 }
-void ResidentStudent::display(int i){
-    cout << "|   " << getStudent() << "    | " << getName() << " |  (" << getFirst() << ")" <<getSecond()<<"-"<<getLast()<<"   |"<< getEmail() << " |\n";
+void ResidentStudent::display() {
+    cout << "|   " << getStudent() << "    | " << getName() << " |  (" << getFirst() << ")" << getSecond() << "-" << getLast() << "   |" << getEmail() << " |\n";
     cout << "---------------------------------------------------\n";
 }
-void InternationalStudent::display(int i){
-    cout << "| " << getStudent() << " | " << getName() << " |  (" << getFirst() << ")" <<getSecond()<<"-"<<getLast()<<"  |"<< getEmail() << " |\n";
+void InternationalStudent::display() {
+    cout << "| " << getStudent() << " | " << getName() << " |  (" << getFirst() << ")" << getSecond() << "-" << getLast() << "  |" << getEmail() << " |\n";
     cout << "---------------------------------------------------\n";
 }
-void LaGuardiaStudent::display(int i){
-    cout << "|   " << getStudent() << "   | " << getName() << " |  (" << getFirst() << ")" <<getSecond()<<"-"<<getLast()<<"   |"<< getEmail() << " |\n";
+void LaGuardiaStudent::display() {
+    cout << "|   " << getStudent() << "   | " << getName() << " |  (" << getFirst() << ")" << getSecond() << "-" << getLast() << "   |" << getEmail() << " |\n";
     cout << "---------------------------------------------------\n";
 }
-void fillFullInfor(ResidentStudent student[]){
-    int index=0;
-    do{
+void fillFullInfor(ResidentStudent student[]) {
+    int index = 0;//index use to concal do while loop
+    do {
         string fullName, emailAdress;
-        int fistNumber,secondNumber,lastNumber;
-        cout<<"Enter your full name:";
-        getline(cin,fullName);
+        int fistNumber, secondNumber, lastNumber;
+        cout << "Enter your full name:";
+        getline(cin, fullName);
         cin.ignore();
         cin.clear();
-        cout<<"Enter your email adress:";
-        getline(cin,emailAdress);
+        cout << "Enter your email adress:";
+        getline(cin, emailAdress);
 
-        cout<<"Enter your phone number in the form(xxx xxx xxxx) \n";
-        cout<<"Please enter first three digit:";
-        cin>>fistNumber;
-        while(getDigit(fistNumber)!=3){
-            cout<<"Invalid input number, plese input(xxx):";
-            cin>>fistNumber;
+        cout << "Enter your phone number in the form(xxx xxx xxxx) \n";
+        cout << "Please enter first three digit:";
+        cin >> fistNumber;
+        //use returned number of index to limit user input digit
+        while (getDigit(fistNumber) != 3) {
+            cout << "Invalid input number, plese input(xxx):";
+            cin >> fistNumber;
         }
-        cout<<"Pleas enter second three digit:";
-        cin>>secondNumber;
-        while(getDigit(secondNumber)!=3){
-            cout<<"Invalid input number, plese input(xxx):";
-            cin>>secondNumber;
+        cout << "Pleas enter second three digit:";
+        cin >> secondNumber;
+        while (getDigit(secondNumber) != 3) {
+            cout << "Invalid input number, plese input(xxx):";
+            cin >> secondNumber;
         }
-        cout<<"Pleasse enter last four digit:";
-        cin>>lastNumber;
-        while(getDigit(lastNumber)!=4){
-            cout<<"Invalid input number, plese input(xxxx):";
-            cin>>lastNumber;
+        cout << "Pleasse enter last four digit:";
+        cin >> lastNumber;
+        while (getDigit(lastNumber) != 4) {
+            cout << "Invalid input number, plese input(xxxx):";
+            cin >> lastNumber;
         }
-
-        student[index].setInformation(fullName,emailAdress,fistNumber,secondNumber,lastNumber);
-        index++;
-    }while(index<SIZE);
-}
-void fillFullInfor(InternationalStudent student[]){
-    int index=0;
-    do{
-        string fullName, emailAdress;
-        int fistNumber,secondNumber,lastNumber;
-        cout<<"Enter your full name:";
-        getline(cin,fullName);
         cin.ignore();
         cin.clear();
-        cout<<"Enter your email adress:";
-        getline(cin,emailAdress);
 
-        cout<<"Enter your phone number in the form(xxxx xxx xxxx) \n";
-        cout<<"Please enter first three digit:";
-        cin>>fistNumber;
-        while(getDigit(fistNumber)!=4){
-            cout<<"Invalid input number, plese input(xxxx):";
-            cin>>fistNumber;
-        }
-        cout<<"Pleas enter second three digit:";
-        cin>>secondNumber;
-        while(getDigit(secondNumber)!=3){
-            cout<<"Invalid input number, plese input(xxx):";
-            cin>>secondNumber;
-        }
-        cout<<"Pleasse enter last four digit:";
-        cin>>lastNumber;
-        while(getDigit(lastNumber)!=4){
-            cout<<"Invalid input number, plese input(xxxx):";
-            cin>>lastNumber;
-        }
-
-        student[index].setInformation(fullName,emailAdress,fistNumber,secondNumber,lastNumber);
+        student[index].setInformation(fullName, emailAdress, fistNumber, secondNumber, lastNumber);
         index++;
-    }while(index<SIZE);
+    } while (index < SIZE);
+}
+void fillFullInfor(InternationalStudent student[]) {
+    int index = 0;
+    do {
+        string fullName, emailAdress;
+        int fistNumber, secondNumber, lastNumber;
+        cout << "Enter your full name:";
+        getline(cin, fullName);
+        cin.ignore();
+        cin.clear();
+        cout << "Enter your email adress:";
+        getline(cin, emailAdress);
+
+        cout << "Enter your phone number in the form(xxxx xxx xxxx) \n";
+        cout << "Please enter first three digit:";
+        cin >> fistNumber;
+        while (getDigit(fistNumber) != 4) {
+            cout << "Invalid input number, plese input(xxxx):";
+            cin >> fistNumber;
+        }
+        cout << "Pleas enter second three digit:";
+        cin >> secondNumber;
+        while (getDigit(secondNumber) != 3) {
+            cout << "Invalid input number, plese input(xxx):";
+            cin >> secondNumber;
+        }
+        cout << "Pleasse enter last four digit:";
+        cin >> lastNumber;
+
+        while (getDigit(lastNumber) != 4) {
+            cout << "Invalid input number, plese input(xxxx):";
+            cin >> lastNumber;
+        }
+        cin.ignore();
+        cin.clear();
+
+        student[index].setInformation(fullName, emailAdress, fistNumber, secondNumber, lastNumber);
+        index++;
+    } while (index < SIZE);
 
 }
