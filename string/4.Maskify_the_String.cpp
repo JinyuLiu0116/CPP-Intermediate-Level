700432cde3d20036cdf78e7d8b5a5e4fceedf11d
Jinyu Liu <149179560+JinyuLiu0116@users.noreply.github.com>
2024-08-26 15:07:18 -0400

    Update 4.Maskify_the_String.cpp

--- string/4.Maskify_the_String.cpp
+++ string/4.Maskify_the_String.cpp
@@ -2,23 +2,25 @@
 #include <string>
 using namespace std;
 
+string maskify(const string& card);
 int main() {
 	string credit_card;
 	cout << "Please enter your cridit card number: ";
 	getline(cin, credit_card);
-	int index = 0;
-	string card_number;
-	if (credit_card.length() <= 4)
-		card_number = credit_card;
-	else{
-		for (int i = 0; i < credit_card.length();i++) {
-			if (i<credit_card.length()-4)
-				card_number.append("#");
-			else{
-				char x = credit_card[i];
-				card_number.push_back(x);
-			}
-		}
+	cin.ignore();
+	while(credit_card.empty()){
+		cout<<"Credit card number cannot be empty\n";
+		cout<<"Please enter your cridit card number: ";
+		getline(cin, credit_card);
+		cin.ignore();
 	}
+	string card_number=maskify(credit_card);
 	cout << "Your card number is:" << card_number << endl;
+}
+string maskify(const string& card){
+	if(card.length()<=4)
+		return card;
+	string m_string(card.length()-4,'#');
+	m_string+=card.substr(card.length()-4);
+	return m_string;
 }
