9cf13c10129a2d8742e5c51b5b7f5527a919050b
Jinyu Liu <149179560+JinyuLiu0116@users.noreply.github.com>
2024-08-29 21:53:28 -0400

    Update Bank_Account_Management.cpp

--- OOP/Bank_Account_Management.cpp
+++ OOP/Bank_Account_Management.cpp
@@ -1,5 +1,6 @@
 #include<iostream>
 #include<sstream>
+#include<ctime>
 using namespace std;
 class BankAccount {
 	int accountNumber;
@@ -30,8 +31,45 @@ public:
 	void setInterestRate(double rate);
 	virtual string displayAccountInfo() override = 0;
 };
+void setAccount(int& num, string& name, double& balance, double& rate);
 int main() {
+	try{
+		char check = 'y';
+		string name;
+		int number=0;
+		double balance = 0, rate = 0;
+		setAccount(number, name, balance, rate);
+		BankAccount account = new SavingAccount(number, name, balance, rate);
+		do{
+		}while(check == 'y');
 
+		delete account;
+	}catch(const invalid_argument e){
+		cerr << "Exception:" << e.what() << endl;
+	}
+}
+
+void setAccount(int& num, string& name, double& balance, double& rate){
+	cout<<"Welcom to ### bank\n";
+	while(name.empty()){
+		cout<<"Please enter your first name:";
+		cin.ignore();
+		getline(cin,name);
+	}
+	srand(time(NULL));
+	int array[16];
+	for(int i = 0; i < 16; i++){
+		array[i] = rand()%10;
+		num = num*10 + array[i];
+	}
+	while(balance <= 0){
+		cout<<"Set your balance:";
+		cin>>balance;
+	}
+	while(rate <= 0){
+		cout<<"Set your rate:";
+		cin>>rate;
+	}
 }
 void SavingAccount::setInterestRate(double rate) {
 	if (rate < 0)
@@ -39,7 +77,6 @@ void SavingAccount::setInterestRate(double rate) {
 	else
 		this->interestRate = rate;
 }
-
 BankAccount::BankAccount(int number, string holderName, double balance) {
 	this->setAccountNumber(number);
 	this->setAccountHolderName(holderName);
