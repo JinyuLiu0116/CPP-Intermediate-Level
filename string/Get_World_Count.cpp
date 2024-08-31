1caee18fe3f71a12005d8a8ce10e0d3f90338fb3
Jinyu Liu <149179560+JinyuLiu0116@users.noreply.github.com>
2024-08-31 13:58:30 -0400

    Add files via upload

---
+++ string/Get_World_Count.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+void setSentence(string& sentence);
+int countWolds(const string& sentence);
+int main() {
+	string sentence;
+	try {
+		setSentence(sentence);
+	}
+	catch (const invalid_argument& e) {
+		cerr << "Exception: " << e.what() << endl;
+	}
+	cout << "The sentence: (" << sentence << ") has " << countWolds(sentence) << " worlds." << endl;
+
+}
+void setSentence(string& sentence) {
+	cout << "Enter a sentence:";
+	getline(cin, sentence);
+	if (sentence.empty()||sentence.at(0)==' ')
+		throw invalid_argument("Sentence cannot be empty!");
+}
+int countWolds(const string& sentence) {
+	int count=1;
+	for (auto it = sentence.begin(); it != sentence.end(); it++) {
+		if (*it == ' ')
+			count ++;
+	}
+	return count;
+}
