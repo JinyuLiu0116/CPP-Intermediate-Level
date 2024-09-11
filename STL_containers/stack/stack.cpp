1ad208ff3eacbbf44ea2efdc697bc4c5c0b33660
Jinyu Liu <149179560+JinyuLiu0116@users.noreply.github.com>
2024-09-10 20:57:51 -0400

    Update stack.cpp

--- STL_containers/stack/stack.cpp
+++ STL_containers/stack/stack.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include <stack>
 
+void printStackElements(std::stack<int> stack);
+
 int main() {
 	std::stack<int>numbersStack;
 
@@ -10,4 +12,17 @@ int main() {
 		std::cout << "Stack is not empty" << std::endl;
 
 	std::cout << "Size of the Stack is:" << numbersStack.size() << std::endl;
+
+	numbersStack.push(15);
+	numbersStack.push(7);
+	numbersStack.push(0);
+
+	printStackElements(numbersStack);
+}
+
+void printStackElements(std::stack<int> stack){
+	while(!stack.empty()){
+		std::cout<<stack.top()<<" "<<std::endl;
+		stack.pop();
+	}
 }
