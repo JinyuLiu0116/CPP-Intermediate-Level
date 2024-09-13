ff7f50b936ab217a4754a650ed62f386b118c014
Jinyu Liu <149179560+JinyuLiu0116@users.noreply.github.com>
2024-09-13 15:18:20 -0400

    Update recursive_function.cpp

--- function/recursive_function.cpp
+++ function/recursive_function.cpp
@@ -2,6 +2,7 @@
 #include <cmath>
 
 void getDivisors(int num);
+int triangleNumber(int num, int sum);
 int main() {
 	int number = 0;
 	std::cout << "Please enter a number and press Enter:";
@@ -11,6 +12,16 @@ int main() {
 	getDivisors(number);
 	std::cout << std::endl;
 
+	int num, sum=0;
+	std::cout << "Please enter a number to caclulate triangle number:";
+	std::cin >> num;
+	while(num <= 0){
+		std::cout<<"Number cannot be 0 or negative.\n";
+		std::cout<<"Please enter number again:";
+		std::cin>>num;
+	}
+	int triangleNum = triangleNumber(num, sum);
+	std::cout<<"The triangle number of "<< num <<" is: "<< triangelNum << std::endl;
 }
 void getDivisors(int num) {
 	for (int i = 2; i <= num; i++) {
@@ -20,4 +31,11 @@ void getDivisors(int num) {
 			return;
 		}
 	}
+}
+int triangleNumber(int num, int sum){
+	sum+=num;
+	if(num != 0)
+		triangleNumber(num-1, sum);
+	else
+		return sum;
 }
