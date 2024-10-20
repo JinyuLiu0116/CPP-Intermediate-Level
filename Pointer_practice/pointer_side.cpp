2f240c9aca659a339d17d21469b79296822463c0
Jinyu Liu <149179560+JinyuLiu0116@users.noreply.github.com>
2024-10-20 19:50:39 -0400

    Update pointer_side.cpp

--- Pointer_practice/pointer_side.cpp
+++ Pointer_practice/pointer_side.cpp
@@ -1,1 +1,11 @@
+#include <iostream>
 
+int main(){
+  int* p_number{}; //will initialize with nullptr
+  double* p_fractional_number{};
+
+  int* p_number1{nullptr};//explicitly initialize with nullptr
+  int* p_fractional_number{nullptr};
+  
+  
+}
