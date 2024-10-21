2608a2ba32e98836c63503cd72b77a3fb27d76f6
Jinyu Liu <149179560+JinyuLiu0116@users.noreply.github.com>
2024-10-20 20:03:41 -0400

    Update pointer_side.cpp

--- Pointer_practice/pointer_side.cpp
+++ Pointer_practice/pointer_side.cpp
@@ -14,4 +14,15 @@ int main(){
   std::cout << "sizeof(int*):" << sizeof(int*) << std::endl; // 8
   std::cout << "sizeof(p_number1):" << sizeof(p_number1) << std::endl; // 8
   std::cout << "sizeof(p_fractional_number1*):" << sizeof(p_fractional_number1) << std::endl; // 8
+
+  // format standard
+  int * p_number2{};
+  int  *p_number3{};
+  int *p_number4{}, another_number4{};
+  int* p_number5{}, another_number5{};
+
+  std::cout << "sizeof(p_number4):" << sizeof(p_number4) << std::endl; // 8
+  std::cout << "sizeof(another_number4):" << sizeof(another_number4) << std::endl; // 4
+  std::cout << "sizeof(p_number5):" << sizeof(p_number5) << std::endl; // 8
+  std::cout << "sizeof(another_number5):" << sizeof(another_number5) << std::endl; // 8
 }
