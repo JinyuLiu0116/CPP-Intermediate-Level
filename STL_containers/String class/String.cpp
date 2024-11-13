c356376c392f71857ad09dd6349d81222636f534
Jinyu Liu <149179560+JinyuLiu0116@users.noreply.github.com>
2024-11-12 21:06:15 -0500

    Update String.cpp

--- STL_containers/String class/String.cpp
+++ STL_containers/String class/String.cpp
@@ -4,5 +4,8 @@
 int main(){
   using namespace std;
   const char* constCStyleString = "Hello String!";
+  count << "Constant string is: " << constCStyleString << endl;
+
+  std::string strFromConst (constCStyleString); //constructor
   
 }
