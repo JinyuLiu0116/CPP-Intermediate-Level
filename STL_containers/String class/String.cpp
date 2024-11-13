8909ae586fe408f41ab28463a5d4705a2c7e9544
Jinyu Liu <149179560+JinyuLiu0116@users.noreply.github.com>
2024-11-12 21:07:45 -0500

    Update String.cpp

--- STL_containers/String class/String.cpp
+++ STL_containers/String class/String.cpp
@@ -7,5 +7,10 @@ int main(){
   count << "Constant string is: " << constCStyleString << endl;
 
   std::string strFromConst (constCStyleString); //constructor
+  cout << "strFromConst is: " << strFromConst << endl;
+
+  std::string str2("Hellow String!");
+  std::string str2Copy(str2);
+  
   
 }
